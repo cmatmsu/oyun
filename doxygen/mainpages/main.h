51c02751e51182207df8b8ce06f1052b76c862cd
Charles Pence <charles@charlespence.net>
2010-10-19 17:13:28 -0400

    Updated Doxygen docs.

---
+++ doxygen/mainpages/main.h
@@ -0,0 +1,18 @@
+/* Main page of the Doxygen documentation */
+
+/**
+
+\mainpage Logos: An evolutionary game-theory simulator
+\author Charles Pence
+\date January 2009
+
+\section code_reference Code Reference:
+
+\li \ref common
+\li \ref game
+\li \ref tourney
+\li \ref ui
+
+*/
+
+
